5f66c9a9d89f4cd04b456e4d699f8ccac7e4bf78
thibo <thibo@LAPTOP-1FMMDL4N>
2021-09-01 12:45:08 +0200

    making some variables and constructor

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class Demo
+    {
+        private String name;
+        private int age;
+
+        public Demo()
+        {
+            name = "Thibo";
+            age = 20;
+;        }
+    }
+}
